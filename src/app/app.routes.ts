d7419a80a73a58d5791c69148f514c897b575f7b
malak <malek.bendhia@fsb.ucar.tn>
2025-03-24 01:27:12 +0000

    home page done!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PatientComponent } from './components/patient/patient.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 export const routes: Routes = [
+    {path:'',component:WelcomePageComponent},
     {path:'patients', component:PatientComponent}
 ];
